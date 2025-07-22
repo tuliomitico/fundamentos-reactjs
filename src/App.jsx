d302dc75724822ec54ff5bbef173362ce5a3cf56
tuliomitico <tulioneedforspeed@gmail.com>
2025-07-22 15:04:00 -0300

    feat: add props on post component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,60 @@ import "./global.css";
 
 import styles from "./App.module.css";
 
+/**
+ * @typedef {Object} Post
+ * @prop {number} id
+ * @prop {Object} author
+ * @prop {string} author.avatarUrl
+ * @prop {string} author.name
+ * @prop {string} author.role
+ * @prop {{ type: "paragraph" | "link", content: string}[]} content
+ * @prop {Date} publishedAt
+ */
+
+/**
+ * @constant
+ * @type {Post[]}
+ */
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/tuliomitico.png",
+      name: "Túlio de Freitas",
+      role: "Fullstack Engineer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-07-15 14:49:01"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/albertveiga.png",
+      name: "Albert Veiga",
+      role: "Severino",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2025-07-22 11:51:35"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,11 +67,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Túlio"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem eligendi veritatis doloribus deleniti laboriosam maiores est sequi aperiam omnis. Molestias ipsum reiciendis officiis error quae mollitia accusamus fuga sequi dolores."
-          />
-          <Post author="Fulano de tal" content="Um post muito legal" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,52 +1,57 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 /**
- * @typedef {Object} Props
- * @prop {string} author
- * @prop {string} content
- */
-
-/**
  *
- * @param {Props} props
+ * @param {import("../App.jsx").Post} props
  * @returns {import("react").JSX.Element}
  */
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    },
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/tuliomitico.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Túlio Mítico</strong>
-            <span>Fullstack Engineer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="15 de Julho às 14:49" datetime="2025-07-15 14:49:01">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          datetime={publishedAt.toISOString()}
+        >
+          Publicado {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>
-          <p>Fala galeraa 👋</p>
-
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-
-          <p>
-            👉 <a href="">jane.design/doctorcare</a>
-          </p>
-
-          <p>
-            <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
-            <a href="">#rocketseat</a>
-          </p>
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p key={line.content}>
+                <a href="">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
